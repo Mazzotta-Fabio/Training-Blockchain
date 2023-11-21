a838b7ad60bbe86675b79a02c237330ad41eff76
Mazzotta-Fabio <33379712+Mazzotta-Fabio@users.noreply.github.com>
2023-11-21 21:52:12 +0100

    nuovo aggiornamento

--- EsempioFacile/contracts/SimpleStorage.sol
+++ EsempioFacile/contracts/SimpleStorage.sol
@@ -2,7 +2,14 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract SimpleStorage {
     uint storedData;
-
+	
+	//funzione fallback per ricevere ether da fonti esterne
+	receive() external payable {}
+			
+	function depositTransfer(address payable _to, uint256 _amount) public{
+			_to.transfer(_amount);
+	}
+	
     function set(uint x) public {
         storedData = x;
     }
